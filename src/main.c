e849f55129f23cd1b2d2ae3880ec1e691c11c979
Zaki Benaissa <zakben45@gmail.com>
2024-02-08 00:20:32 +0100

    Complete 01_Scanner

---
+++ src/main.c
@@ -0,0 +1,53 @@
+#define extern_
+#include "data.h"
+#undef extern_
+#include "defs.h"
+#include "scan.h"
+
+static void init()
+{
+    Line = 1;
+    Putback = '\n';
+}
+
+static void usage(char *prog)
+{
+    printf("Usage: %s <infile>\n", prog);
+    exit(1);
+}
+
+// List of printable tokens
+char *tokstr[] = {"+", "-", "*", "/", "intlit"};
+
+static void scanfile()
+{
+    struct token T;
+
+    while (scan(&T))
+    {
+        printf("Token %s", tokstr[T.token]);
+        if (T.token == T_INTLIT)
+        {
+            printf(", value %d", T.intvalue);
+        }
+        printf("\n");
+    }
+}
+
+void main(int argc, char *argv[])
+{
+    init();
+
+    if (argc != 2)
+    {
+        usage(argv[0]);
+        exit(1);
+    }
+
+    if ((Infile = fopen(argv[1], "r")) == NULL) {
+        printf("Couldn't open file %s\n", argv[1]);
+        exit(1);
+    }
+    scanfile();
+    exit(1);
+}
---
+++ src/scan.c
@@ -0,0 +1,108 @@
+// Get the next character from the input file.
+#include "defs.h"
+#include "scan.h"
+#include "data.h"
+
+static int next(void)
+{
+	int c;
+
+	if (Putback)
+	{
+		c = Putback;
+		Putback = 0;
+		return c;
+	}
+
+	c = fgetc(Infile);
+
+	if ('\n' == c)
+		Line++;
+
+	return c;
+}
+
+// Put back an unwanted character
+static void putback(int c)
+{
+	Putback = c;
+}
+
+// Skip past input we don't need to deal with,
+// i.e. whitespace, newlines. Return the first
+// character we do need.
+static int skip(void)
+{
+	int c;
+
+	c = next();
+	while (' ' == c || '\n' == c || '\t' == c || '\r' == c || '\f' == c)
+	{
+		c = next();
+	}
+
+	return c;
+}
+
+static int chrpos(char *s, int c)
+{
+	char *p;
+
+	p = strchr(s, c);
+	return (p ? p - s : -1);
+}
+
+static int scanint(int c)
+{
+	int k, val = 0;
+
+	while ((k = chrpos("0123456789", c)) >= 0)
+	{
+		val = val * 10 + k;
+		c = next();
+	}
+
+	putback(c);
+	return val;
+}
+
+
+
+int scan(struct token *t)
+{
+	int c;
+
+	c = skip();
+
+	switch (c)
+	{
+	case EOF:
+		return 0;
+		break;
+	case '+':
+		t->token = T_PLUS;
+		break;
+	case '-':
+		t->token = T_MINUS;
+		break;
+	case '*':
+		t->token = T_STAR;
+		break;
+	case '/':
+		t->token = T_SLASH;
+		break;
+	default:
+
+		if (isdigit(c))
+		{
+			t->token = T_INTLIT;
+			t->intvalue = scanint(c);
+			break;
+		}
+
+		printf("Unrecognized character %c on line %d\n", c, Line);
+		exit(1);
+	}
+
+	return 1;
+}
